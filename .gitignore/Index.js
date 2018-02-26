4c9e530fb797f2ad28df5aeb118309804897fc28
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-02-26 15:37:55 +0100

    Update Index.js

--- .gitignore/Index.js
+++ .gitignore/Index.js
@@ -1,6 +1,6 @@
 const { Client } = require('discord.js');
 const yt = require('ytdl-core');
-const ffmpeg = require('ffmpeg')
+const FFMPEG = require('ffmpeg')
 const tokens = require('./tokens.json');
 const client = new Client();
 
