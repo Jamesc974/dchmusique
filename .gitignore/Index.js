8d2b332e67dbb81d19fd2e80bfc00de938548552
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-03-01 15:44:21 +0100

    Update Index.js

--- .gitignore/Index.js
+++ .gitignore/Index.js
@@ -90,7 +90,7 @@ const commands = {
 
 client.on("ready", async () => {
   console.log(`${bot.user.username} est en ligne sur ${client.guilds.size} serveurs!`);
-  client.user.setPresence({ game: { name: `[!dchbot] || connecté à ${client.guilds.size} || créé par TarKyo et DCH`}})
+  client.user.setPresence({ game: { name: `[!dchbot] || connecté à ${client.guilds.size} serveur || créé par TarKyo et DCH`}})
 
 });
 
