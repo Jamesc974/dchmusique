a5e68ac470071545798a3b26050fe5d47e6dc1b8
Jamesc974 <36702319+Jamesc974@users.noreply.github.com>
2018-10-18 15:04:06 +0200

    Update Index.js

--- .gitignore/Index.js
+++ .gitignore/Index.js
@@ -16,6 +16,7 @@ client.on("ready", async () => {
 
 const commands = {
 	'play': (msg) => {
+		if(msg.channel.name == "musique") {
 		if (playlist[msg.guild.id] === undefined) return msg.channel.sendMessage(`Ajoutez quelques chansons à la file d'attente d'abord avec ${tokens.prefix}add`);
 		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
 		if (playlist[msg.guild.id].playing) return msg.channel.sendMessage('❌Musique déjà joué❌');
@@ -63,16 +64,18 @@ const commands = {
 				});
 			});
 		})(playlist[msg.guild.id].songs.shift());
-	},
+	}},
 	'join': (msg) => {
+		if(msg.channel.name == "musique") {
 		return new Promise((resolve, reject) => {
 			const voiceChannel = msg.member.voiceChannel;
 			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply(':disappointed_relieved: Je ne peux pas me connecter à votre Channel');
 			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
 		});
-	},
+	}},
 	'add': (msg) => {
 		let url = msg.content.split(' ')[1];
+		if(msg.channel.name == "musique") {
 		if (url == '' || url === undefined) return msg.channel.sendMessage(`Vous devez ajouter une vidéo YouTube ou id faite ${tokens.prefix}add`);
 		yt.getInfo(url, (err, info) => {
 			if(err) return msg.channel.sendMessage(':x: Lien Youtube Invalide: ' + err);
@@ -81,17 +84,19 @@ const commands = {
 			playlist[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username});
 			msg.channel.sendMessage(`:white_check_mark: Ajout de **${info.title}** à la playlist`);
 		});
-	},
+	}},
 	'playlist': (msg) => {
+		if(msg.channel.name == "musique") {
 		if (playlist[msg.guild.id] === undefined) return msg.channel.sendMessage(`:white_check_mark: Ajoutez quelques chansons à la file d'attente d'abord avec ${tokens.prefix}add`);
 		let tosend = [];
 		playlist[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Par: ${song.requester}`);});
 		msg.channel.sendMessage(`:repeat: __**${msg.guild.name} Playlist :**__ Actuellement **${tosend.length}** Musique juste aprés ${(tosend.length > 15 ? '*[Seulement ensuite 15 montré]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
-	},
+	}},
 	'fm': (msg) => {
+		if(msg.channel.name == "musique") {
 		let tosend = ['```xl', tokens.prefix + 'join : "Pour qu\'il join votre channel"',	tokens.prefix + 'add : "Ajoutez un lien youtube valable à la PlayList"', tokens.prefix + 'playlist : "Montre la file playlist, jusqu\'à 15 chansons montrées."', tokens.prefix + 'play : "Jouez la playList (ATTENTION le bot doit etre dans le channel)"', '', 'Les commandes suivantes fonctionnent seulement si le bot fonctionne :'.toUpperCase(), tokens.prefix + 'pause : "Mettre en pause la musique / arrêter la musique"',	tokens.prefix + 'resume : "Reprend la musique"', tokens.prefix + 'skip : "Changer de Musique"', tokens.prefix + 'time : "Montre la temps de la chanson."',	'volume+(+++) : "Augmente le volume à 2%/+"',	'volume-(---) : "Baisser le volume à 2%/-"',	'```'];
 		msg.channel.sendMessage(tosend.join('\n'));
-	},
+	}},
 	'reboot': (msg) => {
 		if (msg.author.id == tokens.adminID) process.exit(); //Requires a node module like Forever to work.
 	}
